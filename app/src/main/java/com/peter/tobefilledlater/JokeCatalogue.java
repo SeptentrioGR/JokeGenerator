f1dd2d42ceb8a50d7df632a90bd7a4a839c44a92
Petros Kitsaras <seriouspeter@hotmail.com>
2017-01-22 22:36:12 +0200

    Improved

--- app/src/main/java/com/peter/tobefilledlater/JokeCatalogue.java
+++ app/src/main/java/com/peter/tobefilledlater/JokeCatalogue.java
@@ -28,6 +28,15 @@ public class JokeCatalogue {
 
         jokes.add(new Joke("Dad I had Sex for first time"+"Congratulations son. You are a man now. How do you feel?"
         ,"Well... My butt hurts!"));
+
+        jokes.add(new Joke(
+                "A man asks a farmer near a field, “Sorry sir, would you mind if I crossed your field instead of going around it? You see, I have to catch the 4:23 train. \n",
+                "The farmer says, “Sure, go right ahead. And if my bull sees you, you’ll even catch the 4:11 one."));
+
+        jokes.add(new Joke(
+                "Doctor: I'm sorry but you suffer from a terminal illness and have only 10 to live.\n"+
+                        "Patient: What do you mean, 10? 10 what? Months? Weeks?!",
+                "Doctor: Nine."));
     }
 
     public Joke sayRandomJoke(){
--- app/src/main/java/com/peter/tobefilledlater/MainActivity.java
+++ app/src/main/java/com/peter/tobefilledlater/MainActivity.java
@@ -2,50 +2,74 @@ package com.peter.tobefilledlater;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     public static final int DETAIL_REQUEST = 1;
+    TextView appTitle;
     Button mButton = null;
+    ProgressBar mLoadingIndicator;
     TextView jokeTitleText = null;
     TextView jokeDescText = null;
+    LinearLayout jokeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        jokeLayout = (LinearLayout)findViewById(R.id.JokeLayout);
+        appTitle = (TextView)findViewById(R.id.Title_Text);
         jokeTitleText = (TextView)findViewById(R.id.jokeTitle);
         jokeDescText = (TextView)findViewById(R.id.jokeDesc);
-
-
+        mLoadingIndicator = (ProgressBar)findViewById(R.id.loading_indicator);
         mButton = (Button)findViewById(R.id.toSecondActivity);
         mButton.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               Intent i = new Intent(v.getContext(),returnJoke.class);
-               i.putExtra("jokeTitle","Get a joke");
-               startActivityForResult(i,DETAIL_REQUEST);
+               new GenerateJokeTask().execute();
            }
        });
 
 
-
     }
 
-    protected void onActivityResult(int requestCode,int resultCode,Intent data){
-        if(resultCode == RESULT_OK && requestCode == DETAIL_REQUEST){
-            if(data.hasExtra("jokeTitle") && data.hasExtra("jokeDesc")){
-                String titleValue = data.getExtras().getString("jokeTitle");
-                String descValue = data.getExtras().getString("jokeDesc");
-                jokeTitleText.setText(titleValue);
-                jokeDescText.setText(descValue);
-            }
+    public class GenerateJokeTask extends AsyncTask<Void,Void,Joke>{
+
+        @Override
+        protected void onPreExecute() {
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+
+            super.onPreExecute();
         }
+
+        @Override
+        protected Joke doInBackground(Void... voids) {
+            Joke joke = new JokeCatalogue().sayRandomJoke();
+            return joke;
+        }
+
+
+        @Override
+        protected void onPostExecute(Joke joke) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            jokeLayout.setVisibility(View.VISIBLE);
+            appTitle.setVisibility(View.INVISIBLE);
+            if(jokeTitleText.getText().equals(joke.getTitle())){
+                return;
+            }
+            jokeTitleText.setText(joke.getTitle());
+            jokeDescText.setText(joke.getDesc());
+            super.onPostExecute(joke);
+        }
+
     }
 
 }
